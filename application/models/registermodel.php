3b19b1a0fcb59fa5dc24866a30cc93ebe1a59f98
kangfredy <holywarragnarok@gmail.com>
2016-04-25 15:27:48 +0700

    change register model base code igniter
    lebih aman dari sebelumnya dengan base dari CI.

--- application/models/registermodel.php
+++ application/models/registermodel.php
@@ -4,17 +4,22 @@
 Class registermodel extends CI_Model{
     
    function insert_regist(){
-        $name = $_POST['nama'];
         $date = $_POST['tanggal'] . '-' . $_POST['bulan'] . '-' . $_POST['tahun'];
-        $gender = $_POST['jenisKelamin'];
-        $alamatuser = $_POST['alamat'];
-        $mail = $_POST['email'];
-        $no_hp = $_POST['noTelp'];
-        $user= $_POST['usr'];
-        $pswd= $_POST['pwd'];
         
-       $this->db->query("INSERT INTO data_user(Nama_user, jeniskelamin, Email, Tanggal_lahir, Alamat_user, Contac_User, UserLogin, Pass)
-                        VALUES ('$name', '$gender', '$mail', '$date', '$alamatuser', '$no_hp', '$user', '$pswd')");
+        $data = array(
+            'Nama_user' => $this->input->post('nama'),
+            'jenis_kel' => $this->input->post('jenisKelamin'),
+            'Email'=> $this->input->post('email'),
+            'Tanggal_lahir' => $date,
+            'Kecamatan' => $this->input->post('kecamatan'),
+            'Kabupaten' => $this->input->post('kabupaten'),
+            'Provinsi' => $this->input->post('Provinsi'),
+            'Alamat_user' => $this->input->post('alamat'),
+            'Contac_User' => $this->input->post('noTelp'),
+            'Pass'=> $this->input->post('pwd')
+        );
+        
+       $this->db->insert('data_user', $data);
    }
 }
 

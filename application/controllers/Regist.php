11cc314328b3311953c7b453600d91e22666f8c5
kangfredy <holywarragnarok@gmail.com>
2016-04-26 11:09:22 +0700

    controler for json
    controler untuk json mengquery database.

--- application/controllers/Regist.php
+++ application/controllers/Regist.php
@@ -3,21 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Regist extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+	
+
 	public function registform()
 	{
 		$this->load->view('head');
@@ -35,4 +22,33 @@ class Regist extends CI_Controller {
             $this->load->view('Register/regist_success');
             $this->load->view('footer');
         }
+        
+     public function __construct()
+    {
+        parent::__construct();
+        // load from and url helper optional (in this tutorial)
+        $this->load->helper(array(
+            'form',
+            'url'
+        ));
+        // load database
+        $this->load->database();
+        // load form validation library
+        $this->load->library('form_validation');
+    }
+    
+    public function email_check()
+    {
+        // allow only Ajax request    
+        if($this->input->is_ajax_request()) {
+        // grab the email value from the post variable.
+        $email = $this->input->post('email');
+        // check in database - table name : tbl_users  , Field name in the table : email
+        if(!$this->form_validation->is_unique($email, 'data_user.email')) {
+        // set the json object as output                 
+         $this->output->set_content_type('application/json')->set_output(json_encode(array('message' => 'The email is already taken, choose another one')));
+            }
+        }
+    }
+
 }

2fa04ea567ef74d85b94d385912984788654620f
bayuadhitya <badhityaa@gmail.com>
2016-05-15 21:39:36 +0700

    testtt

---
+++ navbar.php
@@ -0,0 +1,53 @@
+<body>
+    
+  <nav class="navbar navbar-default" style="height: 100px; 
+             background-color: #f8f8f8; border:1px solid #f8f8f8; " >
+        <div class="container-fluid">
+            
+        <ul class="nav navbar-nav navbar-left pull-left">
+           
+            <li>
+					<a href="#">
+						<span class="icon-Avatar" style="font-size: 48px; color: #0094cf"> </span>
+					</a>
+			</li>
+					
+             <li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" style="color: #d44f3b; padding-bottom:0px; height:99px"  href="#">
+                            <p class="icon-category" style="font-size: 48px; color: #d44f3b"></p>
+					</a>
+                    <ul class="dropdown-menu" min-width="10px" width="93px" width="93px" style="background-color: #d44f3b"  >
+                        <li><a href="#"><span class="icon-Kategori1" style="font-size: 16px; color: #008bc3"/></a></li>
+                        <li><a href="#">Page 1-2</a></li>
+                        <li><a href="#">Page 1-3</a></li> 
+                    </ul>
+                            
+            </li> 
+            <li><a href="#"><span class="icon-help" style="font-size: 48px; color: #32b6c7"> 
+                    </span></a>
+			</li> 
+        </ul>
+            
+            <a href="<?php echo site_url('Welcome/index') ?>"><span class="brand" style="padding-top: 0px"><img src="<?php echo base_url(); ?>assets/images/Untitled-2.png" width="134px" 
+                                                 height="163px" /></span></a> 
+            
+        <ul class="nav navbar-nav navbar-right pull-right">
+           
+            <li><a  href="#"><span class="icon-search" style="font-size: 48px; color: #32b6c7"> 
+                    </span></a></li>
+            <li>
+				<a href="<?php echo site_url('Login/index') ?>"><span class="icon-login" style="color:#F9B031; font-size:48px;"></span></a>
+			</li>
+        
+            <li align="center">
+                <a href="<?php echo site_url('Regist/registform') ?>"><span class="icon-daftar" style="color:#2F546C; font-size:48px;"></span></a>
+            </li>
+       
+        </ul>
+            
+        </div>
+        </nav>
+           
+	 
+        </ul>
+        

3dcd93d9c0293b592efa2d41a4dc589b77ef28c5
kangfredy <holywarragnarok@gmail.com>
2016-07-27 20:05:46 +0700

    penambahan model
    penambahan model data user toko php

---
+++ application/models/data_user.php
@@ -0,0 +1,79 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class data_user extends CI_models{
+
+    function GetUserID($email){
+        $this->db->select('UserID');
+        $this->db->where('Email', $email);
+        $query = $this->db->get('data_user');
+        return $query->row_array();
+    }
+
+    function GetUserData($email){
+        if($email != FALSE) {
+            $query = $this->db->get_where('data_user', array('Email' => $email));
+            return $query->row_array();
+             }
+        else {
+            return FALSE;
+             }
+        
+    }
+
+    function GetAlamat($email){
+        $this->db->select('Alamat_User');
+        $this->db->where('Email', $email);
+        $query = $this->db->get('data_user');
+        return $query->row_array();
+    }
+
+        
+    function GetPassword($email){
+        $this->db->select('Pass');
+        $this->db->where('Email', $email);
+        $query = $this->db->get('data_user');
+        return $query->row_array();
+    }
+
+
+    function GetContact($email){
+        $this->db->select('Contact_User');
+        $this->db->where('Email', $email);
+        $query = $this->db->get('data_user');
+        return $query->row_array();
+    }
+
+    function GetUrlFoto($email){
+        $this->db->select('Foto_profil');
+        $this->db->where('Email', $email);
+        $query = $this->db->get('data_user');
+        return $query->row_array();
+    }
+
+    function changePasword($email,$password){
+        $this->db->set('Password', $password);
+        $this->db->where('Email', $email);
+        $thos->db->update('data_user');
+        return true;
+    }
+
+    function RegistUser($datauser){
+        $this->db->insert($datauser);
+    }
+
+
+    function ChangeProfile($userid,$datauser){
+        $this->db->where('UserID', $userid);
+        $this->db->update('data_user', $datauser);
+    }
+
+    function ChangeFotoProfile($email,$urlfoto){
+        $this->db->set('Foto_profil', $urlfoto);
+        $this->db->update('data_user');
+    }
+   
+}
+
+
+?>
---
+++ application/models/toko.php
@@ -0,0 +1,64 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class toko extends CI_models{
+    function GetTokoID($userid){
+        $this->db->select('Toko_ID');
+        $this->db->where('UserID', $userid);
+        $query = $this->db->get('toko_order');
+        return $query->row_array();
+     }
+
+     function GetTokoData($userid){
+         $this->db->where('UserID', $userid);
+         $query = $this->db->get('toko_order');
+         return $query->row_array();
+     }
+
+     function setToko($Tokoid, $datatoko){
+         $this->db->where('Toko_ID', $userid);
+         $this->db->update('toko_order', $datatoko);
+         return true;
+        }
+
+     function changeToko($Tokoid, $changedatatoko){
+         $this->db->where('Toko_ID', $userid);
+         $this->db->update('toko_order', $changedatatoko);
+         return true;
+     }
+
+     function changefoto($Tokoid, $urlfoto){
+         $this->db->set('foto_toko', $urlfoto);
+         $this->db->where('Toko_ID', $tokoid);
+         $this->db->update('Fototoko');
+     }
+
+     function GetUrlFotoToko($Tokoid, $urlfoto){
+         $this->db->select('foto_toko');
+         $this->db->where('Toko_ID', $tokoid);
+         $query = $this->db->get('toko_order');
+         $urlfoto = $query->row_array();
+     }
+
+     function GetFotoBackground($Tokoid, $urlbackground){
+         $this->db->select('foto_back');
+         $this->db->where('Toko_ID', $tokoid);
+         $query = $this->db->get('toko_order');
+         $urlbackground = $query->row_array();
+     }
+
+     function DeletebFoto($Tokoid, $urlfoto){
+         $this->db->where('foto_toko', $urlfoto);
+         $this->db->delete('toko_order');
+         return true;
+     }
+
+     function DeletedBackgroundFoto($Tokoid, $urlbackground){
+         $this->db->where('foto_back', $urlbackground);
+         $this->db->delete('toko_order');
+         return true;
+     }
+ 
+
+}
+?>
